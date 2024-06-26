ec6c85f48f9b25fc9552f481d5c0c586015ebd0d
pbl-48 <94658929+PBL-48@users.noreply.github.com>
2024-06-26 12:22:17 +0900

    second

--- q1b/script.js
+++ q1b/script.js
@@ -1,10 +1,10 @@
-function After(){
-    var last = document.getElementById('aft');
-    const daymonth = ["31","28","31","30","31","30","31","31","30","31","30","31"];
-    var m=2;
-    while (last>daymonth[m-1]){
-        last -=daymonth[m-1];
-        m +=1;
+function OnButtonClick(){
+    var last = 4 + 87;
+    const daymonth = ["31", "28", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31"];
+    var m = 2;
+    while (last > daymonth[m - 1]) {
+        last -= daymonth[m - 1];
+        m += 1;
     }
-    document.getElementById('result').innerHTML=String(m)+"月"+String(last)+"日";
+    document.getElementById('result').innerHTML = String(m) + "月" + String(last) + "日";
 }
--- q2/script.js
+++ q2/script.js
@@ -1,19 +1,12 @@
-const weekday=["日","月","火","水","木","金","土",]
+const weekday=["日","月","火","水","木","金","土"];
+
 function After(){
-    console.log(document.getElementById('y').value,document.getElementById('m').value-1,document.getElementById('d').value)
-    let date=new Date(document.getElementById('y').value,document.getElementById('m').value-1,document.getElementById('d').value);
-    console.log(date);
-    date.setDate(date.getDate()+Number(document.getElementById('input').value));
-    console.log(document.getElementById('input').value);
-    console.log(date);
-    document.getElementById('result').innerHTML = String(date.getFullYear())+"年"+String(date.getMonth()+1)+"月"+String(date.getDate())+"日"+String(weekday[date.getDay()])+"曜日";
+    let date=new Date(document.getElementById('y').value, document.getElementById('m').value - 1, document.getElementById('d').value);
+    date.setDate(date.getDate() + Number(document.getElementById('input').value));
+    document.getElementById('result').innerHTML = String(date.getFullYear()) + "年" + String(date.getMonth() + 1) + "月" + String(date.getDate()) + "日" + String(weekday[date.getDay()]) + "曜日";
 }
 function Before(){
-    console.log(document.getElementById('y').value,document.getElementById('m').value-1,document.getElementById('d').value)
-    let date=new Date(document.getElementById('y').value,document.getElementById('m').value-1,document.getElementById('d').value);
-    console.log(date);
-    date.setDate(date.getDate()-Number(document.getElementById('input').value));
-    console.log(document.getElementById('input').value);
-    console.log(date);
-    document.getElementById('result').innerHTML = String(date.getFullYear())+"年"+String(date.getMonth()+1)+"月"+String(date.getDate())+"日"+String(weekday[date.getDay()])+"曜日";
+    let date=new Date(document.getElementById('y').value, document.getElementById('m').value - 1, document.getElementById('d').value);
+    date.setDate(date.getDate() - Number(document.getElementById('input').value));
+    document.getElementById('result').innerHTML = String(date.getFullYear()) + "年" + String(date.getMonth() + 1) + "月" + String(date.getDate()) + "日" + String(weekday[date.getDay()]) + "曜日";
 }
